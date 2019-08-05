b9a14c0fc4bee616f1a9e6eb105d0047fb93b723
wanga26 <alex.wang@emc.com>
2019-08-05 12:39:21 +0800

    Add selection sort

--- 11_sorts/sorting.py
+++ 11_sorts/sorting.py
@@ -32,8 +32,28 @@ def insertion_sort(sorting_list):
                 sorting_list[j+1] = sorting_list[j]
             else:
                 break
+        # here using the python for-else trick, since there is not j-- in for loop like in C language
+        else:
+            j = -1
 
-        sorting_list[j] = next_value
+        sorting_list[j+1] = next_value
+
+# selection sort
+# Ascending way
+def selection_sort(sorting_list):
+    length = len(sorting_list)
+    for i in range(length):
+        min_index = i
+        for j in range(i+1, length):
+            if sorting_list[j] < sorting_list[min_index]:
+                min_index = j
+
+        if min_index != i:
+            # swap i-th element with min_index: j
+            tmp = sorting_list[i]
+            sorting_list[i] = sorting_list[min_index]
+            sorting_list[min_index] = tmp
+
 
 
 test_list = [6, 5, 4, 3, 2, 1]
@@ -42,4 +62,8 @@ print(test_list)
 
 test_list = [2, 3, 1]
 insertion_sort(test_list)
+print(test_list)
+
+test_list = [6, 5, 4, 3, 2, 1]
+selection_sort(test_list)
 print(test_list)
