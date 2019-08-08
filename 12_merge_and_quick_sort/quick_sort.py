953a464fc70413f27a4798d04ba3a652b489db57
unknown <buptwxd@163.com>
2019-08-08 22:38:16 +0800

    Finish quick sort

--- 12_merge_and_quick_sort/quick_sort.py
+++ 12_merge_and_quick_sort/quick_sort.py
@@ -1,30 +1,44 @@
 # This is the quick sort
 # And its core is the partition function
 
-def partition(list_to_sort):
-    start = 0
-    end = len(list_to_sort) - 1
+def partition(list_to_sort, start, end):
     pivot = list_to_sort[end]
     pvt_idx = start  # indicating the final index of pivot
 
-    for i in range(start, end):  # i indicating the index whose value less than pivot
+    for i in range(start, end):  # [0, pvt_idx-1] is the list whose values less than pivot.和选择排序类似
         if list_to_sort[i] <= pivot:
             # swap list_to_sort[i] and list_to_sort[pvt_idx]
-            tmp = list_to_sort[i]
-            list_to_sort[i] = list_to_sort[pvt_idx]
-            list_to_sort[pvt_idx] = tmp
+            swap(list_to_sort, i, pvt_idx)
             pvt_idx += 1
+
+    swap(list_to_sort, pvt_idx, end)
 
     return pvt_idx
 
-def quick_sort(list_to_sort):
+
+def swap(my_list, i, j):
+    if i == j:
+        return
+
+    tmp = my_list[i]
+    my_list[i] = my_list[j]
+    my_list[j] = tmp
+
+    return
+
+def quick_sort(list_to_sort, start, end):
+    # if start >= end:
+    #     return
+
     if len(list_to_sort) == 0 or len(list_to_sort) == 1:
         return
 
-    pvt_idx = partition(list_to_sort)
-    quick_sort(list_to_sort[0:pvt_idx-1])
-    quick_sort(list_to_sort[pvt_idx+1:])
+    pvt_idx = partition(list_to_sort, start, end)
+    quick_sort(list_to_sort, start, pvt_idx-1)  # slice is a copy
+    quick_sort(list_to_sort, pvt_idx+1, end)    # slice is a copy
+
 
 list_a = [6, 4, 5, 7, 3, 2, 10, 1]
-quick_sort(list_a)
+quick_sort(list_a, 0, len(list_a)-1)
 print(list_a)
+
